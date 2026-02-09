750499d4d4a46bc6922b08fb25b4eafe6e324685
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-09 22:24:38 +0530

    Deploying Page Objects

--- Scripts/TestScripts.py
+++ Scripts/TestScripts.py
@@ -1,4 +1,6 @@
-{Import necessary modules}
+import asyncio
+from RuleConfigurationPage import RuleConfigurationPage
+from LoginPage import LoginPage
 
 class TestLoginFunctionality:
     def __init__(self, page):
@@ -12,4 +14,55 @@ class TestLoginFunctionality:
 
     async def test_remember_me_functionality(self):
         await self.login_page.navigate()
-        await self.login_page.fill_email('
+        await self.login_page.fill_email('')
+        # Add remaining implementation here
+
+class TestRuleConfiguration:
+    def __init__(self, page):
+        self.page = page
+        self.rule_page = RuleConfigurationPage(page)
+
+    async def test_TC_SCRUM158_01_create_and_validate_rule(self):
+        # Prepare JSON rule schema with all supported trigger, condition, and action types
+        rule_schema = {
+            "name": "AllTypesRule",
+            "trigger": {"type": "all_supported_trigger", "params": {}},
+            "conditions": [
+                {"type": "condition_type_1", "params": {}},
+                {"type": "condition_type_2", "params": {}}
+            ],
+            "actions": [
+                {"type": "action_type_1", "params": {}},
+                {"type": "action_type_2", "params": {}}
+            ]
+        }
+        await self.rule_page.navigate_to_rule_configuration()
+        await self.rule_page.open_create_rule_dialog()
+        await self.rule_page.input_rule_schema(rule_schema)
+        await self.rule_page.submit_rule()
+        assert await self.rule_page.verify_rule_created(rule_schema["name"])
+        assert await self.rule_page.validate_rule_schema(rule_schema)
+
+    async def test_TC_SCRUM158_02_create_rule_multiple_conditions_actions_and_simulate(self):
+        # Prepare rule schema with multiple conditions/actions
+        rule_schema = {
+            "name": "MultiCondActionRule",
+            "trigger": {"type": "supported_trigger", "params": {}},
+            "conditions": [
+                {"type": "condition_type_1", "params": {}},
+                {"type": "condition_type_2", "params": {}},
+                {"type": "condition_type_3", "params": {}}
+            ],
+            "actions": [
+                {"type": "action_type_1", "params": {}},
+                {"type": "action_type_2", "params": {}},
+                {"type": "action_type_3", "params": {}}
+            ]
+        }
+        await self.rule_page.navigate_to_rule_configuration()
+        await self.rule_page.open_create_rule_dialog()
+        await self.rule_page.input_rule_schema(rule_schema)
+        await self.rule_page.submit_rule()
+        assert await self.rule_page.verify_rule_created(rule_schema["name"])
+        await self.rule_page.simulate_rule(rule_schema["name"])
+        assert await self.rule_page.verify_simulation_results(rule_schema["name"])
