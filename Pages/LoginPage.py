31a40ea8a40acd3c55b7d427e55bd02463a97c3d
Ascend-Krithi <gkiruthika1505@gmail.com>
2026-02-09 15:21:59 +0530

    Deploying Page Objects

--- Pages/LoginPage.py
+++ Pages/LoginPage.py
@@ -1,8 +1,10 @@
+import selenium.webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 class LoginPage:
-    def __init__(self, driver: WebDriver):
+    def __init__(self, driver):
         self.driver = driver
         self.url = "https://example-ecommerce.com/login"
         self.email_field = (By.ID, "login-email")
@@ -19,33 +21,51 @@ class LoginPage:
     def open(self):
         self.driver.get(self.url)
 
-    def login(self, email: str, password: str, remember_me: bool = False):
-        self.driver.find_element(*self.email_field).clear()
-        self.driver.find_element(*self.email_field).send_keys(email)
-        self.driver.find_element(*self.password_field).clear()
-        self.driver.find_element(*self.password_field).send_keys(password)
-        checkbox = self.driver.find_element(*self.remember_me_checkbox)
-        if remember_me != checkbox.is_selected():
-            checkbox.click()
-        self.driver.find_element(*self.login_submit).click()
+    def enter_email(self, email):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.email_field)).clear()
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.email_field)).send_keys(email)
+
+    def enter_password(self, password):
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.password_field)).clear()
+        WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.password_field)).send_keys(password)
+
+    def click_remember_me(self):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.remember_me_checkbox)).click()
+
+    def click_login(self):
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.login_submit)).click()
 
     def click_forgot_password(self):
-        self.driver.find_element(*self.forgot_password_link).click()
+        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(self.forgot_password_link)).click()
 
     def get_error_message(self):
-        return self.driver.find_element(*self.error_message).text
+        try:
+            return WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.error_message)).text
+        except:
+            return None
 
     def get_validation_error(self):
-        return self.driver.find_element(*self.validation_error).text
+        try:
+            return WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.validation_error)).text
+        except:
+            return None
 
-    def is_empty_field_prompt_visible(self):
-        elements = self.driver.find_elements(*self.empty_field_prompt)
-        return len(elements) > 0 and elements[0].is_displayed()
+    def get_empty_field_prompt(self):
+        try:
+            return WebDriverWait(self.driver, 5).until(EC.visibility_of_element_located(self.empty_field_prompt)).text
+        except:
+            return None
 
     def is_dashboard_header_visible(self):
-        elements = self.driver.find_elements(*self.dashboard_header)
-        return len(elements) > 0 and elements[0].is_displayed()
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.dashboard_header))
+            return True
+        except:
+            return False
 
     def is_user_profile_icon_visible(self):
-        elements = self.driver.find_elements(*self.user_profile_icon)
-        return len(elements) > 0 and elements[0].is_displayed()
+        try:
+            WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(self.user_profile_icon))
+            return True
+        except:
+            return False
